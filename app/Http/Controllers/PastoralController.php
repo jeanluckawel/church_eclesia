9700a4da3d9d435b4cb7840de7220c40a4ad35cb
jeanluckawel <20ka208@esisalama.org>
2026-07-16 19:45:36 +0200

    Add functionality for managing active and inactive members

---
+++ app/Http/Controllers/PastoralController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Pastoral;
+use Illuminate\Http\Request;
+
+class PastoralController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Pastoral $pastoral)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Pastoral $pastoral)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Pastoral $pastoral)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Pastoral $pastoral)
+    {
+        //
+    }
+}
