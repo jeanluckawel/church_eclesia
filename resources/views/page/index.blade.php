c64191546be652bf1069a8701020310ce6d79b7c
jeanluckawel <20ka208@esisalama.org>
2026-07-17 07:55:10 +0200

    Uncomment CSS stylesheet link in page index.

--- resources/views/page/index.blade.php
+++ resources/views/page/index.blade.php
@@ -14,7 +14,7 @@
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
   <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,500;0,600;0,700;1,500&family=Plus+Jakarta+Sans:ital,wght@0,400;0,500;0,600;0,700;1,400&display=swap" rel="stylesheet" />
-{{--  <link rel="stylesheet" href="{{asset('css/site.css') }}" />--}}
+  <link rel="stylesheet" href="{{asset('css/site.css') }}" />
 </head>
 <body>
   <a class="site-skip" href="#contenu">Aller au contenu</a>
